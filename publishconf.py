efc4734aaa40484ba39d1a6623e7f1876cd5685e
Nick Foti <nfoti01@gmail.com>
2013-08-15 13:08:34 -0700

    publishconf.py: Change server target to ~nfoti/rn.

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://cs.dartmouth.edu/~nfoti/rb'
+SITEURL = 'http://cs.dartmouth.edu/~nfoti/rn'
 RELATIVE_URLS = True
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
