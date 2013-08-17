134d13a621e142954f2a1969361ab48bc57564e8
Nick Foti <nfoti01@gmail.com>
2013-08-16 17:02:47 -0700

    new-post.py includes date in created filename.

--- new-post.py
+++ new-post.py
@@ -42,7 +42,7 @@ def main():
 
     # Use the slug as the filename so no spaces or weird characters in
     # filenames.
-    filename = "%s.md" % (args.slug,)
+    filename = "%s-%s.md" % (date.date(), args.slug,)
     filename = os.path.join(args.path, filename)
     filename = os.path.expanduser(filename)
 
